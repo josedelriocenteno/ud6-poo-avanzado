eb7c8d5abe1854f8b790c3b34997f98a3fff4563
josedelriocenteno <josemiguel.delrio.centeno@gregoriofer.com>
2026-01-12 17:03:40 +0100

    Inicial commit Ud6 POO Avanzado

---
+++ src/main/java/com/github/josedelriocenteno/ud6/Ud6PooAvanzado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.josedelriocenteno.ud6;
+
+/**
+ *
+ * @author delcenjo
+ */
+public class Ud6PooAvanzado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
